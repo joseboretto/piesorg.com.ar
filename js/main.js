ffd17a11f2f9d40a26cac2ce3cc8c3f7ac1f3c55
juan_gimenez <gimenezjuan92@gmail.com>
2016-07-24 23:23:43 -0300

    cambios en js y offset bien

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 
+$(document).ready(function () {
+    $("#botonDonaciones").click(function () {
+        $("#textoDonaciones").show();
+        $("#textoAN").hide();
+        $("#textoIPV").hide();
+
+
+        $('html, body').animate({
+            scrollTop: $("#textoDonaciones").offset().top
+        }, 1000);
+    });
+
+    $("#botonAN").click(function () {
+        $("#textoAN").show();
+        $("#textoDonaciones").hide();
+        $("#textoIPV").hide();
+
+
+        $('html, body').animate({
+            scrollTop: $("#textoAN").offset().top-120
+        }, 1000);
+    });
+
+    $("#botonIPV").click(function () {
+        $("#textoIPV").show();
+        $("#textoAN").hide();
+        $("#textoDonaciones").hide();
+
+
+        $('html, body').animate({
+            scrollTop: $("#textoIPV").offset().top-120
+        }, 1000);
+    });
+
+});
+
