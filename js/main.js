775dea45834b1ffc345c63455c89ccd9b4f46220
JOSE <joseboretto@gmail.com>
2016-08-17 20:04:41 -0300

    nuevas diseno en imagenes, cambio de margenes y toggles arreglados

--- js/main.js
+++ js/main.js
@@ -1,37 +1,26 @@
-
-$(document).ready(function () {
+(document).ready(function () {
     $("#botonDonaciones").click(function () {
-        $("#textoDonaciones").show();
+        $("#textoDonaciones").toggle();
         $("#textoAN").hide();
         $("#textoIPV").hide();
-
-
         $('html, body').animate({
             scrollTop: $("#textoDonaciones").offset().top
         }, 1000);
     });
-
     $("#botonAN").click(function () {
-        $("#textoAN").show();
+        $("#textoAN").toggle();
         $("#textoDonaciones").hide();
         $("#textoIPV").hide();
-
-
         $('html, body').animate({
-            scrollTop: $("#textoAN").offset().top-120
+            scrollTop: $("#textoAN").offset().top - 120
         }, 1000);
     });
-
     $("#botonIPV").click(function () {
-        $("#textoIPV").show();
+        $("#textoIPV").toggle();
         $("#textoAN").hide();
         $("#textoDonaciones").hide();
-
-
         $('html, body').animate({
-            scrollTop: $("#textoIPV").offset().top-120
+            scrollTop: $("#textoIPV").offset().top - 120
         }, 1000);
     });
-
 });
-
