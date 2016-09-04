47165399d5edc2ac54c7c7a52968f944d587f611
JOSE <joseboretto@gmail.com>
2016-09-04 00:21:24 -0300

    Lista de cambio luego de mostrarle a meli,  la lista de cambios esta en bersion 2 cmabios, se rompio el olverglox en x y no se como arregklarlo, el scroll no anda

---
+++ PaginaPIES/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $("#botonAN").click(function () {
+        $("#textoAN").toggle();
+        $("#textoDonaciones").hide();
+        $("#textoIPV").hide();
+        $('html, body').animate({
+            scrollTop: $('#textoAN').offset().top
+        }, 'slow');
+        return false;
+    });
+    $("#botonIPV").click(function () {
+        $("#textoIPV").toggle();
+        $("#textoAN").hide();
+        $("#textoDonaciones").hide();
+        $('html, body').animate({
+            scrollTop: $("#textoIPV").offset().top - 120
+        }, 1000);
+    });
+    $("#botonDonaciones").click(function () {
+        $("#textoDonaciones").toggle();
+        $("#textoAN").hide();
+        $("#textoIPV").hide();
+        $('html, body').animate({
+            scrollTop: $("#textoDonaciones").offset().top - 120
+        }, 1000);
+    });
+});
